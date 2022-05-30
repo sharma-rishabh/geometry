5696364098e24b808303ca6f726a4fac9759efb7
Risabh Sharma <99526730+sharma-rishabh@users.noreply.github.com>
2022-05-30 10:11:36 +0530

    Has now basic properties like length and slope.

---
+++ line.js
@@ -0,0 +1,30 @@
+const square = x => x * x;
+
+class Line {
+  constructor(startPoint, endPoint) {
+    this.startPoint = startPoint;
+    this.endPoint = endPoint;
+  }
+
+  deltaY() {
+    return this.endPoint.y - this.startPoint.y;
+  }
+
+  deltaX() {
+    return this.endPoint.x - this.startPoint.x;
+  }
+
+  length() {
+    const base = square(this.deltaX());
+    const height = square(this.deltaY());
+    return Math.sqrt(base + height);
+  }
+
+  slopeInRad() {
+    return Math.atan(this.slope());
+  }
+
+  slope() {
+    return (this.deltaY()) / (this.deltaX());
+  };
+}
