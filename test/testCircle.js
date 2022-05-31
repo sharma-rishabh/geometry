d9edbcfb2276b3f53e2a68c3a50b455653f207f9
Risabh Sharma <99526730+sharma-rishabh@users.noreply.github.com>
2022-05-31 12:28:52 +0530

    Added toHtml test cases for circle.

--- test/testCircle.js
+++ test/testCircle.js
@@ -12,4 +12,22 @@ describe('Circle', () => {
     assert.ok(circle1.equals(circle2));
     assert.strictEqual(circle1.equals(circle3), false);
   });
+
+  it('should write html code to generate given circle.', () => {
+    const origin = new Point(4, 5);
+    const circle1 = new Circle(origin, 5);
+
+    const html = '<div style="width:10px;height:10px;border-radius:50%;border:1px solid black;position:absolute;top: 0px;left: -1px;"></div>';
+
+    assert.strictEqual(circle1.toHtml(), html)
+  });
+
+  it('should write html code to generate given circle from origin.', () => {
+    const origin = new Point(0, 0);
+    const circle1 = new Circle(origin, 5);
+
+    const html = '<div style="width:10px;height:10px;border-radius:50%;border:1px solid black;position:absolute;top: -5px;left: -5px;"></div>';
+
+    assert.strictEqual(circle1.toHtml(), html)
+  });
 });
